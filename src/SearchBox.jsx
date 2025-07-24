3cb5894dc91fe31b3cc37cfc45c8b6caacea49a4
sukhmeet <sukhmeetsingh645@gmail.com>
2025-07-25 00:58:09 +0530

    search box design

---
+++ src/SearchBox.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import "./SearchBox.css";
+
+export default function SearchBox() {
+    let [city, setCity] = useState("");
+
+    let handleChange = (evt) => {
+        setCity(evt.target.value);
+    };
+
+    let handleSubmit = (evt) => {
+        evt.preventDefault();
+        console.log(city);
+        setCity("");
+    };
+  return (
+    <div className="SearchBox">
+      <h3>Seach for weather</h3>
+      <form onSubmit={handleSubmit}>
+        <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
+        <br /> <br />
+        <Button variant="contained" type="submit">
+          Search
+        </Button>
+      </form>
+    </div>
+  );
+}
