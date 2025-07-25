843585dd2da488ca66e2cdc9622566a944496ffb
sukhmeet <sukhmeetsingh645@gmail.com>
2025-07-25 11:55:45 +0530

    Api added

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,22 +4,47 @@ import Button from "@mui/material/Button";
 import "./SearchBox.css";
 
 export default function SearchBox() {
-    let [city, setCity] = useState("");
+  let [city, setCity] = useState("");
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  const API_KEY = "ffef519454a7c7f578563917e351bc48";
 
-    let handleChange = (evt) => {
-        setCity(evt.target.value);
-    };
+  let getWeatherInfo = async () => {
+    let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+    let jsonResponse = await response.json();
+    console.log(jsonResponse);
+    let result = {
+        temp: jsonResponse.main.temp,
+        tempMin: jsonResponse.main.temp_min,
+        tempMax: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        feelsLike: jsonResponse.main.feelsLike,
+        weather: jsonResponse.weather[0].description,
+    }
+    console.log(result);
+  };
 
-    let handleSubmit = (evt) => {
-        evt.preventDefault();
-        console.log(city);
-        setCity("");
-    };
+  let handleChange = (evt) => {
+    setCity(evt.target.value);
+  };
+
+  let handleSubmit = (evt) => {
+    evt.preventDefault();
+    console.log(city);
+    setCity("");
+    getWeatherInfo();
+  };
   return (
     <div className="SearchBox">
       <h3>Seach for weather</h3>
       <form onSubmit={handleSubmit}>
-        <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
+        <TextField
+          id="city"
+          label="City Name"
+          variant="outlined"
+          required
+          value={city}
+          onChange={handleChange}
+        />
         <br /> <br />
         <Button variant="contained" type="submit">
           Search
